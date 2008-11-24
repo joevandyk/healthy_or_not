3678ead6128f9d6060e918b4a2976a95b2c76f0d
Jordan Isip <jordanisip@yahoo.com>
2008-11-24 01:01:39 -0800

    Adding favicon

--- wp-content/themes/healthy-or-not/header.php
+++ wp-content/themes/healthy-or-not/header.php
@@ -9,6 +9,9 @@
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/favicon.ico" />
+
+
 <?php wp_head(); ?>
 <?php
 if ( is_home() || is_single() ) { $current = 'reviews'; }
